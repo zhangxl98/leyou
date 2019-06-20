a5fb170995cc45067316d72d3a370d68f312a3f3
zhangxl98 <651268723@qq.com>
2019-06-20 10:27:43 +0800

    创建 Zuul 网关

---
+++ ly-gateway/src/main/java/com/leyou/gateway/LyGateway.java
@@ -0,0 +1,23 @@
+package com.leyou.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @Author zhangxl98
+ * @Date 6/20/19 10:15 AM
+ * @OS Ubuntu 18.04 LTS
+ * @Device ASRock-Desktop
+ * @Version V1.0.0
+ * @Description Zuul 网关启动类
+ */
+@SpringCloudApplication
+@EnableZuulProxy
+public class LyGateway {
+    public static void main(String[] args) {
+        SpringApplication.run(LyGateway.class);
+    }
+}
