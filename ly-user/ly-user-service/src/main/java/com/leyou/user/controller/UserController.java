42bdfe067653edf942295e01bcaa88119613ad6b
zhangxl98 <651268723@qq.com>
2019-07-13 21:16:08 +0800

    使用 Swagger UI 生成 API 文档

--- ly-user/ly-user-service/src/main/java/com/leyou/user/controller/UserController.java
+++ ly-user/ly-user-service/src/main/java/com/leyou/user/controller/UserController.java
@@ -3,6 +3,10 @@ package com.leyou.user.controller;
 import com.leyou.user.dto.UserDTO;
 import com.leyou.user.entity.User;
 import com.leyou.user.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -36,9 +40,14 @@ public class UserController {
      * @return true：可用；false：不可用；200：校验成功；400：参数有误；500：服务器内部异常
      */
     @GetMapping("/check/{data}/{type}")
+    @ApiOperation(value = "校验用户名数据是否可用，如果不存在则可用")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "校验结果有效，true 或 false 代表可用或不可用"),
+            @ApiResponse(code = 400, message = "请求参数有误，比如 type 不是指定值")
+    })
     public ResponseEntity<Boolean> check(
-            @PathVariable String data,
-            @PathVariable Integer type
+            @ApiParam(value = "要校验的数据", example = "jack") @PathVariable("data") String data,
+            @ApiParam(value = "数据类型，1：用户名，2：手机号", example = "1") @PathVariable(value = "type") Integer type
     ) {
         return ResponseEntity.ok(userService.check(data, type));
     }
